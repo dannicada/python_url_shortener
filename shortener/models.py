08df3ffdd40938a5e13942348d1e8ad0d6e89d42
dannicda <dannynwangwu@gmail.com>
2019-12-29 22:54:26 +0100

    added shortURLManager class to models

--- shortener/models.py
+++ shortener/models.py
@@ -4,13 +4,31 @@ from django.db import models
 
 from .utils import code_generator, create_shortcode
 
+class shortURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(shortURLManager,self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = shortURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
 
 class shortURL(models.Model):
     url = models.CharField(max_length = 220,)
     shortcode = models.CharField(max_length = 15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    objects = shortURLManager()
+    #some_random = shortURLManager()
     
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":
