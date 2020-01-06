be3616a9bafcc82c8ec93dbf6ad3fd58e4ce0ae3
dannicada <dannynwangwu@gmail.com>
2020-01-06 16:35:47 +0100

    both class based view and function based view now set up to handle shortcode urls

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import shortURL
@@ -7,22 +7,27 @@ from .models import shortURL
 
 #function based view
 def short_redirect_View(request, shortcode=None, *args, **kwargs):
-    # print(request.user)
-    # print(request.user.is_authenticated())
     # obj = shortURL.objects.get(shortcode=shortcode)
+
+    obj = get_object_or_404(shortURL, shortcode=shortcode)
+    # obj_url = obj.url
+
     # try:
     #     obj = shortURL.objects.get(shortcode=shortcode)
     # except:
     #     obj = shortURL.objects.all().first()
-    obj_url = None
-    qs = shortURL.objects.filter(shortcode__iexact=shortcode.upper())
-    if qs.exists() and qs.count() == 1:
-        obj = qs.first()
-        obj_url = obj.url
+    # obj_url = None
+    # qs = shortURL.objects.filter(shortcode__iexact=shortcode.upper())
+    # if qs.exists() and qs.count() == 1:
+    #     obj = qs.first()
     return HttpResponse("hello {sc}".format(sc = obj.url))
 
 
 #class based view
 class shortCBView(View): 
     def get(self, request, shortcode=None, *args,**kwargs):
-        return HttpResponse("hello again {sc}".format(sc=shortcode))
+        obj = get_object_or_404(shortURL, shortcode=shortcode)
+        return HttpResponse("hello again {sc}".format(sc=obj.url))
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse();
